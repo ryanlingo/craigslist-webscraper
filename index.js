2006cfa780589beb7a5ee0ffcbba1f997518c24c
Ryan Lingo <ryanlingo@gmail.com>
2021-10-30 08:42:00 -0400

    refactor main func by including helper functions

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
-async function main() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+async function scrapeListings(page) {
   await page.goto(
     "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof"
   );
@@ -11,7 +9,7 @@ async function main() {
   const html = await page.content();
   const $ = cheerio.load(html);
 
-  const results = $(".result-info")
+  const listings = $(".result-info")
     .map((index, element) => {
       const titleElement = $(element).find(".result-title");
       const timeElement = $(element).find(".result-date");
@@ -27,7 +25,25 @@ async function main() {
       return { title, url, datePosted, hood };
     })
     .get();
-  console.log(results);
+  return listings;
+}
+
+async function scrapeJobDescriptions(listings, page) {
+  for (let i = 0; i < listings.length; i++) {
+    await page.goto(listings[i].url);
+    const html = await page.content();
+  }
+}
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const listings = await scrapeListings(page);
+  const listingsWithJobDescriptions = await scrapeJobDescriptions(
+    listings,
+    page
+  );
+  console.log(listings);
 }
 
 main();
